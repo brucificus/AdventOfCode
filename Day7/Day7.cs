9fed516bdd6da884cdf3fd415b6a9b172603502e
Bruce Markham <219281+brucificus@users.noreply.github.com>
2020-12-07 17:05:53 -0500

    🗓 Day 7, Part 1

--- Day7/Day7.cs
+++ Day7/Day7.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using QuickGraph;
+using QuickGraph.Algorithms;
 using Shared;
 
 public class Day7
@@ -11,7 +13,12 @@ public class Day7
     private IReadOnlyList<string> inputLines;
     public record BagColor(string value);
     public record BagGrouping(int count, BagColor color);
-    public record BagContentsPattern(BagColor color, IReadOnlyCollection<BagGrouping> bagGroupings);
+    public record BagGraphEdge(BagColor inward, BagColor outward, int count) : QuickGraph.IEdge<BagColor>
+    {
+        BagColor IEdge<BagColor>.Source => this.inward;
+
+        BagColor IEdge<BagColor>.Target => this.outward;
+    }
 
     [SetUp]
     public async Task Setup()
@@ -19,8 +26,8 @@ public class Day7
         inputLines = await System.IO.File.ReadAllLinesAsync("input.txt");
     }
 
-    [Test]
-    public void Part1()
+    [Test(ExpectedResult = 274)]
+    public int Part1()
     {
         var arrow = new Regex("\\sbags\\scontain\\s");
         var nonEmptyBagTrailer = new Regex("(\\d+)\\s((\\w+\\s)+)bags?(\\.|,\\s)");
@@ -29,18 +36,57 @@ public class Day7
         {
             foreach (var match in matches.Cast<Match>())
             {
-                yield return new BagGrouping(int.Parse(match.Groups[1].Value), new BagColor(match.Groups[2].Value));
+                yield return new BagGrouping(int.Parse(match.Groups[1].Value), new BagColor(match.Groups[2].Value.Trim()));
             }
         }
 
         var parsedInputLines =
             (from line in inputLines
              let lineSides = line.Split(arrow)
+             let inwardBagColor = new BagColor(lineSides[0].Trim())
              let emptyBagTrailerMatch = lineSides[1].Matches(emptyBagTrailer).SingleOrDefault()
              let nonEmptyBagTrailerMatches = lineSides[1].Matches(nonEmptyBagTrailer)
-             select emptyBagTrailerMatch != null
-                    ? new BagContentsPattern(new BagColor(lineSides[0]), ImmutableList<BagGrouping>.Empty)
-                    : new BagContentsPattern(new BagColor(lineSides[0]), CaptureBagGroupings(nonEmptyBagTrailerMatches).ToImmutableList()))
-            .ToImmutableList();
+             let outwardBagColorsAndQuantities = emptyBagTrailerMatch != null
+                    ? ImmutableList<BagGrouping>.Empty
+                    : CaptureBagGroupings(nonEmptyBagTrailerMatches).ToImmutableList()
+            select (inwardBagColor, outwardBagColorsAndQuantities))
+             .ToImmutableDictionary(i => i.inwardBagColor, i => i.outwardBagColorsAndQuantities);
+
+        var uniqueBagColors = parsedInputLines.Aggregate(parsedInputLines.Keys.ToImmutableHashSet(), (p, c) => p.Union(c.Value.Select(i => i.color)));
+        var bagEdges = (from inward in uniqueBagColors
+                        where parsedInputLines.ContainsKey(inward)
+                        let outwardsAndQuantities = parsedInputLines[inward]
+                        let edges = outwardsAndQuantities.Select(o => new BagGraphEdge(inward, o.color, o.count)).ToImmutableList().AsEnumerable()
+                        from edge in edges
+                        select edge)
+                       .ToImmutableHashSet();
+
+        IEnumerable<BagColor> FindAllBagColorsThatCanContainBagColor(BagColor desiredBagColor)
+        {
+            var bagGraphBuilder = new AdjacencyGraph<BagColor, BagGraphEdge>();
+            bagGraphBuilder.AddVertexRange(uniqueBagColors);
+            bagGraphBuilder.AddVerticesAndEdgeRange(bagEdges);
+            var bagGraph = bagGraphBuilder.ToBidirectionalGraph();
+
+            var results = ImmutableHashSet<BagColor>.Empty;
+            foreach (var root in uniqueBagColors)
+            {
+                var searchFunction = bagGraph.TreeBreadthFirstSearch(root);
+                searchFunction(desiredBagColor, out IEnumerable<BagGraphEdge> resultEdges);
+                resultEdges ??= ImmutableList<BagGraphEdge>.Empty;
+                var allEdgesExceptLast = resultEdges.Take(resultEdges.Count() - 1);
+                var lastEdge = resultEdges.Skip(resultEdges.Count() - 1).SingleOrDefault();
+                results = allEdgesExceptLast.Aggregate(results, (p, c) => p.Add(c.inward).Add(c.outward));
+                if (lastEdge != null)
+                {
+                    results = results.Add(lastEdge.inward);
+                }
+            }
+
+            return results;
+        }
+
+        var allBagColorsThatCanContainBagColor = FindAllBagColorsThatCanContainBagColor(new BagColor("shiny gold"));
+        return allBagColorsThatCanContainBagColor.Count();
     }
 }
