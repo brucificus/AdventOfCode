7ae01b539cf63515c0688843a1373cbf4aa00ad4
Bruce Markham <219281+brucificus@users.noreply.github.com>
2020-12-08 09:10:35 -0500

    🗓 Day 8, Part 2

--- Day8/Day8.cs
+++ Day8/Day8.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OneOf;
+using InstructionOneOf = OneOf.OneOf<Instructions.AccInstruction, Instructions.JmpInstruction, Instructions.NopInstruction>;
 
 public class Day8
 {
@@ -16,68 +17,98 @@ public class Day8
         inputLines = await System.IO.File.ReadAllLinesAsync("input.txt");
     }
 
-    [Test(ExpectedResult = -1)]
+    [Test(ExpectedResult = 1384)]
     public int Part1()
     {
         var instructions = inputLines.Where(l => !string.IsNullOrWhiteSpace(l)).Select((l, i) => Instructions.ParseLine(i, l)).ToImmutableList();
 
-        int? FindAccumulatorAtFirstCycle()
+        var executionResult = Execute(instructions);
+
+        return executionResult.Match(
+            normal => throw new InvalidOperationException(),
+            cycled => cycled.accumulator);
+    }
+
+    [Test(ExpectedResult = 761)]
+    public int Part2()
+    {
+        var originalInstructions = inputLines.Where(l => !string.IsNullOrWhiteSpace(l)).Select((l, i) => Instructions.ParseLine(i, l)).ToImmutableList();
+
+        static InstructionOneOf Invert(InstructionOneOf instruction)
         {
-            var instructionsExecuted = new HashSet<Instructions.Instruction>();
-            int accumulator = 0;
-
-            for (var executionIndex = 0; executionIndex < instructions.Count; executionIndex++)
-            {
-                var instruction = instructions[executionIndex];
-                if (instructionsExecuted.Contains((Instructions.Instruction)instruction.Value))
-                {
-                    return accumulator;
-                }
-                else
-                {
-                    instructionsExecuted.Add((Instructions.Instruction)instruction.Value);
-                }
-
-                instruction.Switch(
-                    acc => accumulator += acc.argument,
-                    jmp => executionIndex = (executionIndex + jmp.argument) - 1,
-                    _ => { });
-            }
-
-            return null;
+            return instruction.Match<InstructionOneOf>(
+                acc => acc,
+                jmp => new Instructions.NopInstruction(jmp.index, jmp.argument),
+                nop => new Instructions.JmpInstruction(nop.index, nop.argument));
         }
 
-        return FindAccumulatorAtFirstCycle().Value;
+        for (var instructionIndexToChange = 0; instructionIndexToChange < originalInstructions.Count; instructionIndexToChange++)
+        {
+            var invertedInstruction = Invert(originalInstructions[instructionIndexToChange]);
+            var newInstructionSet = originalInstructions
+                .RemoveAt(instructionIndexToChange)
+                .Insert(instructionIndexToChange, invertedInstruction);
+
+            NormalTermination? terminationFound = null;
+            var executionResult = Execute(newInstructionSet);
+            executionResult.Switch(normal => terminationFound = normal, cycled => { });
+            if (terminationFound != null)
+            {
+                return terminationFound.accumulator;
+            }
+        }
+
+        Assert.Fail();
+        return -1;
     }
+
+    public static OneOf<NormalTermination, CycledTermination> Execute(ImmutableList<InstructionOneOf> instructions)
+    {
+        var instructionsExecuted = new HashSet<Instructions.Instruction>();
+        int accumulator = 0;
+
+        for (var executionIndex = 0; executionIndex < instructions.Count; executionIndex++)
+        {
+            var instruction = instructions[executionIndex];
+            if (instructionsExecuted.Contains((Instructions.Instruction)instruction.Value))
+            {
+                return new CycledTermination(accumulator, (Instructions.Instruction)instruction.Value);
+            }
+            else
+            {
+                instructionsExecuted.Add((Instructions.Instruction)instruction.Value);
+            }
+
+            instruction.Switch(
+                acc => accumulator += acc.argument,
+                jmp => executionIndex = (executionIndex + jmp.argument) - 1,
+                _ => { });
+        }
+
+        return new NormalTermination(accumulator);
+    }
+
+    public record NormalTermination(int accumulator);
+    public record CycledTermination(int accumulator, Instructions.Instruction revisitedInstruction);
 }
 
 public static class Instructions
 {
-    public static OneOf<AccInstruction, JmpInstruction, NopInstruction> ParseLine(int index, string value)
+    public static InstructionOneOf ParseLine(int index, string value)
     {
         var instructionArgument = int.Parse(value.Substring(4));
         return value.Substring(0, 3) switch
         {
             "acc" => new AccInstruction(index, instructionArgument),
             "jmp" => new JmpInstruction(index, instructionArgument),
-            "nop" => new NopInstruction(index),
+            "nop" => new NopInstruction(index, instructionArgument),
             _ => throw new ArgumentException(nameof(value))
         };
     }
 
-    public record Instruction(int index);
+    public record Instruction(int index, int argument);
 
-    public record ParameterizedInstruction : Instruction
-    {
-        public int argument { get; init; }
-
-        public ParameterizedInstruction(int index, int argument) : base(index)
-        {
-            this.argument = argument;
-        }
-    }
-
-    public record AccInstruction : ParameterizedInstruction
+    public record AccInstruction : Instruction
     {
         public AccInstruction(int index, int argument)
             : base(index, argument)
@@ -85,7 +116,7 @@ public static class Instructions
         }
     }
 
-    public record JmpInstruction : ParameterizedInstruction
+    public record JmpInstruction : Instruction
     {
         public JmpInstruction(int index, int argument)
             : base(index, argument)
@@ -95,8 +126,8 @@ public static class Instructions
 
     public record NopInstruction : Instruction
     {
-        public NopInstruction(int index)
-            : base(index)
+        public NopInstruction(int index, int argument)
+            : base(index, argument)
         {
         }
     }
