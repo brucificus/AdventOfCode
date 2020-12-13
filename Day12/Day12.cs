c4218e4e34eea9e458ee12c64fd5ef0fe83c7935
Bruce Markham <219281+brucificus@users.noreply.github.com>
2020-12-12 21:50:42 -0500

    🗓 Day 12, Part 2

--- Day12/Day12.cs
+++ Day12/Day12.cs
@@ -2,23 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using MotionInstruction = OneOf.OneOf<Day12.MoveNorth, Day12.MoveSouth, Day12.MoveEast, Day12.MoveWest, Day12.TurnLeft, Day12.TurnRight, Day12.MoveForward>;
+using OrientationAdjustmentInstruction = OneOf.OneOf<Day12.MoveNorth, Day12.MoveSouth, Day12.MoveEast, Day12.MoveWest, Day12.TurnLeft, Day12.TurnRight, Day12.MoveForward>;
 
 public class Day12
 {
+    private static readonly float PiOver180 = (float)(Math.PI / 180d);
+
     private IReadOnlyCollection<string> inputLines;
 
-    public record MoveNorth(int distance);
-    public record MoveSouth(int distance);
-    public record MoveEast(int distance);
-    public record MoveWest(int distance);
-    public record TurnLeft(int degrees);
-    public record TurnRight(int degrees);
-    public record MoveForward(int distance);
+    public record MoveNorth(float distance);
+    public record MoveSouth(float distance);
+    public record MoveEast(float distance);
+    public record MoveWest(float distance);
+    public record TurnLeft(float degrees);
+    public record TurnRight(float degrees);
+    public record MoveForward(float distance);
 
-    public record BoatOrientation(int x, int y, int rotation);
+    public record Orientation(Vector2 position, float rotation);
+    public record Velocity(Orientation orientation, float waypointDistance);
 
     [SetUp]
     public async Task Setup()
@@ -29,54 +33,131 @@ public class Day12
     [Test(ExpectedResult = 1838)]
     public int Part1()
     {
-        var initialOrientation = new BoatOrientation(0, 0, CompassDegreesToCircleDegrees(90));
+        var initialBoatOrientation = new Orientation(Vector2.Zero, CompassDegreesToCircleDegrees(90));
 
         var motionInstructions = ParseInput();
 
-        var finalOrientation = motionInstructions.Aggregate(initialOrientation, ApplyMotion);
+        var finalBoatOrientation = motionInstructions.Aggregate(initialBoatOrientation, ApplyPart1OrientationAdjustment);
 
-        return Math.Abs(finalOrientation.x) + Math.Abs(finalOrientation.y);
+        return ManhattenDistanceFromOrigin(finalBoatOrientation.position);
     }
 
-    private static readonly double PiOver180 = Math.PI / 180f;
-    public BoatOrientation ApplyMotion(BoatOrientation startOrientation, MotionInstruction instruction)
+    [Test(ExpectedResult = 89936)]
+    public int Part2()
     {
-        static double DegreesToRadians(int degrees)
+        var initialBoatPosition = Vector2.Zero;
+        var initialRelativeWaypointPosition = new Vector2(10, 1);
+        var initialRelativeWaypointBearing = CartesianToPolar(initialRelativeWaypointPosition);
+        var startVelocity = new Velocity(new Orientation(initialBoatPosition, initialRelativeWaypointBearing.angleDegrees), initialRelativeWaypointBearing.magnitude);
+
+        var motionInstructions = ParseInput();
+
+        var finalVelocity = motionInstructions.Aggregate(startVelocity, ApplyPart2OrientationAdjustment);
+
+        return ManhattenDistanceFromOrigin(finalVelocity.orientation.position);
+    }
+
+    public Velocity ApplyPart2OrientationAdjustment(Velocity startVelocity, OrientationAdjustmentInstruction instruction)
+    {
+        Velocity TranslateWaypointPosition(Vector2 offset)
         {
-            return degrees * PiOver180;
+            var newWaypointBearing = CartesianToPolar(PolarToCartesian(startVelocity.orientation.rotation, startVelocity.waypointDistance) + offset);
+            return new Velocity(new Orientation(startVelocity.orientation.position, newWaypointBearing.angleDegrees), newWaypointBearing.magnitude);
         }
 
-        var (x, y, rotation) = startOrientation;
-        var resultOrientation = instruction.Match(
-                moveNorth => new BoatOrientation(x, y + moveNorth.distance, rotation),
-                moveSouth => new BoatOrientation(x, y - moveSouth.distance, rotation),
-                moveEast => new BoatOrientation(x + moveEast.distance, y, rotation),
-                moveWest => new BoatOrientation(x - moveWest.distance, y, rotation),
-                turnRight => new BoatOrientation(x, y, (rotation + turnRight.degrees) % 360),
-                turnLeft => new BoatOrientation(x, y, (rotation - turnLeft.degrees) % 360),
-                moveForward =>
-                {
-                    var newX = x + (int)Math.Round(Math.Cos(DegreesToRadians(rotation)) * moveForward.distance);
-                    var newY = y + (int)Math.Round(Math.Sin(DegreesToRadians(rotation)) * moveForward.distance);
-                    return new BoatOrientation(newX, newY, rotation);
-                });
+        Velocity RotateWaypointAroundPosition(float angleDegrees)
+        {
+            var newWaypointBearing = (angleDegrees: startVelocity.orientation.rotation + angleDegrees, distance: startVelocity.waypointDistance);
+            return new Velocity(new Orientation(startVelocity.orientation.position, newWaypointBearing.angleDegrees), newWaypointBearing.distance);
+        }
+
+        Velocity MoveTowardsWaypoint(float magnitude)
+        {
+            var waypointRelativeCoordinate = PolarToCartesian(startVelocity.orientation.rotation, startVelocity.waypointDistance);
+            var newPosition = startVelocity.orientation.position + (waypointRelativeCoordinate * magnitude);
+            return new Velocity(new Orientation(newPosition, startVelocity.orientation.rotation), startVelocity.waypointDistance);
+        }
+
+        var resultVelocity = instruction.Match(
+            moveNorth => TranslateWaypointPosition(new Vector2(0, moveNorth.distance)),
+            moveSouth => TranslateWaypointPosition(new Vector2(0, -moveSouth.distance)),
+            moveEast => TranslateWaypointPosition(new Vector2(moveEast.distance, 0)),
+            moveWest => TranslateWaypointPosition(new Vector2(-moveWest.distance, 0)),
+            turnLeft => RotateWaypointAroundPosition(turnLeft.degrees),
+            turnRight => RotateWaypointAroundPosition(-turnRight.degrees),
+            moveForward => MoveTowardsWaypoint(moveForward.distance));
+
+        Print(startVelocity, instruction, resultVelocity);
+
+        return resultVelocity;
+    }
+
+    public Orientation ApplyPart1OrientationAdjustment(Orientation startOrientation, OrientationAdjustmentInstruction instruction)
+    {
+        var startPosition = startOrientation.position;
+        var rotation = startOrientation.rotation;
+
+        var resultPosition = startPosition + instruction.Match(
+                moveNorth => new Vector2(0, moveNorth.distance),
+                moveSouth => new Vector2(0, -moveSouth.distance),
+                moveEast => new Vector2(moveEast.distance, 0),
+                moveWest => new Vector2(-moveWest.distance, 0),
+                turnLeft => Vector2.Zero,
+                turnRight => Vector2.Zero,
+                moveForward => PolarToCartesian(rotation, moveForward.distance));
+        var resultRotation = rotation + instruction.Match(
+                moveNorth => 0,
+                moveSouth => 0,
+                moveEast => 0,
+                moveWest => 0,
+                turnLeft => turnLeft.degrees,
+                turnRight => -turnRight.degrees,
+                moveForward => 0);
+
+        var resultOrientation = new Orientation(resultPosition, resultRotation);
         Print(startOrientation, instruction, resultOrientation);
+
         return resultOrientation;
     }
 
-    public int CompassDegreesToCircleDegrees(int degrees)
+    public static int ManhattenDistanceFromOrigin(Vector2 position) =>
+        (int)Math.Round(Math.Abs(position.X) + Math.Abs(position.Y));
+
+    public static float CompassDegreesToCircleDegrees(float degrees) =>
+        degrees - 90;
+
+    public static float DegreesToRadians(float degrees) =>
+        degrees * PiOver180;
+
+    public static float RadiansToDegrees(float radians) =>
+        radians / PiOver180;
+
+    public static Vector2 PolarToCartesian(float angleDegrees, float magnitude)
     {
-        return degrees - 90;
+        var angleRadians = DegreesToRadians(angleDegrees);
+        return new Vector2(
+            (float)Math.Cos(angleRadians) * magnitude,
+            (float)Math.Sin(angleRadians) * magnitude);
     }
 
-    public IReadOnlyList<MotionInstruction> ParseInput()
+    public static (float angleDegrees, float magnitude) CartesianToPolar(Vector2 coordinate)
+    {
+        var angleRadians = Math.Atan2(coordinate.Y, coordinate.X);
+
+        var angleDegrees = (float)RadiansToDegrees((float)angleRadians);
+        var magnitude = (float)Math.Sqrt(Math.Pow(coordinate.X, 2) + Math.Pow(coordinate.Y, 2));
+
+        return (angleDegrees, magnitude);
+    }
+
+    public IReadOnlyList<OrientationAdjustmentInstruction> ParseInput()
     {
         return (from line in inputLines
                where !string.IsNullOrWhiteSpace(line) && line.Length > 0
                let action = line[0]
                let actionParameter = int.Parse(line[1..])
 #pragma warning disable SA1119 // Statement should not use unnecessary parenthesis
-                select (MotionInstruction)(action switch
+                select (OrientationAdjustmentInstruction)(action switch
                {
                    'N' => new MoveNorth(actionParameter),
                    'S' => new MoveSouth(actionParameter),
@@ -90,13 +171,18 @@ public class Day12
 #pragma warning restore SA1119 // Statement should not use unnecessary parenthesis
     }
 
-    private void Print(BoatOrientation boatOrientationIn, MotionInstruction motionInstruction, BoatOrientation boatOrientationOut)
+    private void Print(Velocity velocityIn, OrientationAdjustmentInstruction orientationAdjustment, Velocity velocityOut)
     {
-        static string FormatOrientation(BoatOrientation bo) => $"({bo.x: 000;-000}, {bo.y: 000;-000})@{bo.rotation: 000;-000}°";
+        static string FormatVelocity(Velocity v)
+        {
+            var o = v.orientation;
+            var cb = PolarToCartesian(v.orientation.rotation, v.waypointDistance);
+            return $"({o.position.X: 000;-000}, {o.position.Y: 000;-000})->({cb.X: 00;-00}, {cb.Y: 00;-00})";
+        }
 
-        TestContext.Out.Write(FormatOrientation(boatOrientationIn));
+        TestContext.Out.Write(FormatVelocity(velocityIn));
         TestContext.Out.Write(" \t");
-        TestContext.Out.Write(motionInstruction.Match(
+        TestContext.Out.Write(orientationAdjustment.Match(
                 moveNorth => $"N{moveNorth.distance:00}",
                 moveSouth => $"S{moveSouth.distance:00}",
                 moveEast => $"E{moveEast.distance:00}",
@@ -105,6 +191,24 @@ public class Day12
                 turnRight => $"R{turnRight.degrees:000}",
                 moveForward => $"F{moveForward.distance:00}"));
         TestContext.Out.Write(" \t");
-        TestContext.Out.WriteLine(FormatOrientation(boatOrientationOut));
+        TestContext.Out.WriteLine(FormatVelocity(velocityOut));
+    }
+
+    private void Print(Orientation orientationIn, OrientationAdjustmentInstruction orientationAdjustment, Orientation orientationOut)
+    {
+        static string FormatOrientation(Orientation o) => $"({o.position.X: 000;-000}, {o.position.Y: 000;-000})@{o.rotation: 000;-000}°";
+
+        TestContext.Out.Write(FormatOrientation(orientationIn));
+        TestContext.Out.Write(" \t");
+        TestContext.Out.Write(orientationAdjustment.Match(
+                moveNorth => $"N{moveNorth.distance:00}",
+                moveSouth => $"S{moveSouth.distance:00}",
+                moveEast => $"E{moveEast.distance:00}",
+                moveWest => $"W{moveWest.distance:00}",
+                turnLeft => $"L{turnLeft.degrees:000}",
+                turnRight => $"R{turnRight.degrees:000}",
+                moveForward => $"F{moveForward.distance:00}"));
+        TestContext.Out.Write(" \t");
+        TestContext.Out.WriteLine(FormatOrientation(orientationOut));
     }
 }
