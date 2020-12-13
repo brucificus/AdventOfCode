fd66d67ca52059acb5ace82894c4c9ce446a07fa
Bruce Markham <219281+brucificus@users.noreply.github.com>
2020-12-13 17:09:17 -0500

    🗓 Day 13, Part 2
    I had the technique for this spoiled for me, for which I'm grateful: I've never heard of the Chinese Remainder Theorem and I doubt I would have found out about it in a reasonable amount of time. (My brute-force attempts at this were non-productive.)
    
    https://www.wikiwand.com/en/Chinese_remainder_theorem#Search_by_sieving

--- Day13/Day13.cs
+++ Day13/Day13.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using OneOf;
 
 public class Day13
 {
+    public record Any;
     private IReadOnlyList<string> inputLines;
 
     [SetUp]
@@ -17,7 +21,7 @@ public class Day13
     [Test(ExpectedResult = 171)]
     public int Part1()
     {
-        var (earliestTimestamp, busIds) = ParseInput();
+        var (earliestTimestamp, busIds) = ParseInputForPart1();
 
         var busArrivals = busIds
                             .Select(b => (busId: b, delay: (((earliestTimestamp / b) + 1) * b) - earliestTimestamp))
@@ -28,10 +32,54 @@ public class Day13
         return nextArrival.busId * nextArrival.delay;
     }
 
-    private (int earliestTimestamp, IReadOnlyList<int> busIds) ParseInput()
+    [Test(ExpectedResult = "539746751134958")]
+    public string Part2()
+    {
+        var busIdPatterns = ParseInputForPart2();
+
+        var busIdOffsets =
+            (from offset in Enumerable.Range(0, busIdPatterns.Count)
+             let busIdPattern = busIdPatterns[offset]
+             where busIdPattern.Match(_ => true, _ => false)
+             let busId = (BigInteger)busIdPattern.Match(n => n, _ => throw new InvalidOperationException())
+             select (offset, busId))
+            .ToImmutableArray();
+
+        BigInteger position = 0;
+        BigInteger productOfFoundBusIds = busIdOffsets.First().busId;
+
+        foreach (var (offset, busId) in busIdOffsets.Skip(1))
+        {
+            while ((position + offset) % busId != 0)
+            {
+                position += productOfFoundBusIds;
+            }
+
+            productOfFoundBusIds *= busId;
+        }
+
+        return position.ToString();
+    }
+
+    private (int earliestTimestamp, IReadOnlyList<int> busIds) ParseInputForPart1()
     {
         return (
             int.Parse(inputLines[0]),
             inputLines[1].Split(',').Where(i => i != "x").Select(int.Parse).ToImmutableList());
+    }
+
+    private IReadOnlyList<OneOf<int, Any>> ParseInputForPart2()
+    {
+        return inputLines[1]
+            .Split(',')
+            .Select(v => (OneOf<int, Any>)
+#pragma warning disable SA1119 // Statement should not use unnecessary parenthesis
+                (v switch {
+                    "x" => new Any(),
+                    string n => int.Parse(n),
+                    _ => throw new System.InvalidOperationException()
+            }))
+#pragma warning restore SA1119 // Statement should not use unnecessary parenthesis
+            .ToImmutableList();
     }
 }
