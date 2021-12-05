a3872666b3585c98d533160c4801ec2662c4d22c
Bruce Markham <219281+brucificus@users.noreply.github.com>
2021-12-05 07:13:51 -0500

    🛣 Preparation for Day 5
    Creates the project, pastes in the starting files for it.

---
+++ src/Y2021/Day05/Program.cs
@@ -0,0 +1,35 @@
+using TPart1InputParsed = System.Collections.Generic.IReadOnlyList<Undefined>;
+using TPart1Answer = Undefined;
+using TPart2InputParsed = System.Collections.Generic.IReadOnlyList<Undefined>;
+using TPart2Answer = Undefined;
+
+
+await NUnitApplication.CreateBuilder().Build().RunAsync();
+
+
+[TestFixture(Description = "Day05")]
+public partial class Program : TestableSolverBase<TPart1InputParsed, TPart1Answer, TPart2InputParsed, TPart2Answer>
+{
+    protected override TPart1InputParsed ParseInputForPart1(IReadOnlyList<string> lines) =>
+        lines.WhereNot(string.IsNullOrEmpty).Select(_ => default(Undefined));
+
+    protected override TPart1Answer Part1AnswerSample => throw new NotImplementedException("Part 1 Sample Answer");
+    protected override TPart1Answer Part1AnswerActual => throw new NotImplementedException("Part 1 Actual Answer");
+
+    protected override TPart1Answer Part1Solver(TPart1InputParsed input)
+    {
+        throw new NotImplementedException("Part 1 Solver");
+    }
+
+
+
+    protected override TPart2InputParsed ParseInputForPart2(IReadOnlyList<string> lines) => default!;
+
+    protected override TPart2Answer Part2AnswerSample => throw new NotImplementedException("Part 2 Sample Answer");
+    protected override TPart2Answer Part2AnswerActual => throw new NotImplementedException("Part 2 Actual Answer");
+
+    protected override TPart2Answer Part2Solver(TPart2InputParsed input)
+    {
+        throw new NotImplementedException("Part 2 Solver");
+    }
+}
